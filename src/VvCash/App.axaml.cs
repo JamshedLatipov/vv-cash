5425198546aa8bad579f9faa75a3215fd8ef1b67
Jamshed Latipov <jamshedlatipov@gmail.com>
2026-06-07 00:20:57 +0500

    feat: register ReturnService in DI with AuthHeaderHandler

--- src/VvCash/App.axaml.cs
+++ src/VvCash/App.axaml.cs
@@ -146,6 +146,7 @@ public partial class App : Application
         services.AddHttpClient<IShiftService, ShiftService>().AddHttpMessageHandler<AuthHeaderHandler>();
         services.AddHttpClient<ExpenseDocumentService>().AddHttpMessageHandler<AuthHeaderHandler>();
         services.AddSingleton<IExpenseDocumentService>(sp => sp.GetRequiredService<ExpenseDocumentService>());
+        services.AddHttpClient<IReturnService, ReturnService>().AddHttpMessageHandler<AuthHeaderHandler>();
         services.AddHttpClient<ISyncService, SyncService>().AddHttpMessageHandler<AuthHeaderHandler>();
 
         // POS Services
